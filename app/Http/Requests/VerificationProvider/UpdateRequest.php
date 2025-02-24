b4a252e06a2d78f34958305e0fa099b7b54ccb13
chanchal <chanchal.hipl@gmail.com>
2025-02-24 17:01:36 +0530

    name spacing issue resolve

---
+++ app/Http/Requests/VerificationProvider/UpdateRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\VerificationProvider;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        
+        return [
+            'name' => 'required|string|unique:provider_types,name,' . $this->verification_provider->id . '|max:255',
+            'description' => 'nullable|string',
+            'country_id' => 'required|exists:countries,id',
+            'provider_type_id' => 'required|exists:provider_types,id',
+            'contact_address' => 'nullable|string',
+            'email' => [
+                'required',
+                'email',
+                'unique:verification_providers,email,' . $this->verification_provider->id, 
+                'regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', 
+            ],
+            'website' => 'nullable|url',
+            'contact_person' => 'nullable|string|max:255',
+            'status' => 'required|in:1,0',
+        ];
+    }
+}
+
+
