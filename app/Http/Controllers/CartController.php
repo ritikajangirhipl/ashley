a3a851cbcdd1ac84a5d1580ab790ccc4e2a6980b
manishajain123 <manishajain.his@gmail.com>
2025-02-24 13:22:42 +0530

    order form frontend

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function addToCart(Request $request)
+    {
+        dd($request);
+
+    }
+
+}
